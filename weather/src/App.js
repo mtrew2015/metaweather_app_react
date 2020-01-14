ce60e8cadc7a1652f2affc4b179243e2969b2b0b
Michael Trew <mtrew2015@gmail.com>
2020-01-13 22:39:15 -0500

    made axios call and pass the data to MainPage.js

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import MainPage from './components/MainPage/MainPage';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Weather</h1>
-     <MainPage/>
-    </div>
-  );
+	const [ data, setData ] = useState({});
+
+	useEffect(() => {
+		const id = '2487956';
+		axios
+			.get(`https://weatherapi.mtrew.now.sh/api/getweather?id=${id}`)
+			.then((res) => setData(res.data))
+			.catch((err) => console.log(err));
+	}, []);
+	return (
+		<div className='App'>
+			<h1>Weather</h1>
+			<MainPage data={data} />
+		</div>
+	);
 }
 
 export default App;
